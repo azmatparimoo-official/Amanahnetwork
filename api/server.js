9bb55f780d4f1472949d1694096fe921e527d7f0
azmatparimoo <9149azmat@gmail.com>
2026-06-12 16:44:12 +0530

    payment32

--- api/server.js
+++ api/server.js
@@ -115,8 +115,10 @@ app.get('/api/verify/:token', async (req, res) => {
 
 // --- PAYMENT INTEGRATION ---
 app.post('/api/payment/create-order', async (req, res) => {
-  const { amount, donorEmail, projectTitle , donorName , mobileNumber } = req.body;
-
+  const { amount, donorEmail, projectTitle , donorName , mobileNumber , agreedToTerms } = req.body;
+ if (!agreedToTerms) {
+  return res.status(400).json({ error: "You must agree to the Audit Terms." });
+  }
   // Validation: Prevent empty requests from crashing the server
   if (!amount || !donorEmail || !donorName || !mobileNumber) {
     return res.status(400).json({ error: "Missing required donation details" });
